3f76b51d906146af50459c00bc28e5147fc946a4
Neil A. <11821288+ImpossibleCodes@users.noreply.github.com>
2026-01-18 19:19:09 -0500

    Update and rename brew-casks.txt to install_brew-casks.sh

---
+++ install_brew-casks.sh
@@ -0,0 +1,20 @@
+# Develoopment apps
+brew install --cask visual-studio-code
+brew install --cask balenaetcher
+brew install openconnect
+
+# CAD apps
+brew install --cask autodesk-fusion
+brew install --cask orcaslicer
+brew install --cask kicad
+
+# Misc apps
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask spotify
+brew install --cask vlc
+
+# Social apps
+brew install --cask discord
+brew install --cask minecraft
+
